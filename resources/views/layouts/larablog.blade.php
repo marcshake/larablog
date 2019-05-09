8f61f0657298057233d80b362f1f346b9e7eb11a
Marcel Schindler <marcel.schindler@lobo-tueren.de>
2019-05-09 11:47:40 +0200

    Layout added, seeder added

---
+++ resources/views/layouts/larablog.blade.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="{{ asset('css/app.css')}}">
+    <title>Document</title>
+</head>
+
+<body>
+    <nav class="navbar is-light" role="navigation" aria-label="main navigation">
+        <div class="container">
+            <div class="navbar-brand">
+                <a class="navbar-item" href="{{ url('/') }}">
+                    <img src="https://www.trancefish.de/assets/tlog.svg.svg"
+                        alt="TLOG5 - Laravel based Blogging Solution" height="16">
+                </a>
+
+                <a role="button" class="navbar-burger" aria-label="menu" aria-expanded="false">
+                    <span aria-hidden="true"></span>
+                    <span aria-hidden="true"></span>
+                    <span aria-hidden="true"></span>
+                </a>
+            </div>
+            <div class="navnar-menu" id="navMenu">
+                <div class="navbar-start">
+                    <a href="{{ url('/')}}" class="navbar-item">Home</a>
+                    <a href="{{ url('blog/')}}" class="navbar-item">Blog</a>
+                </div>
+            </div>
+
+            <div class="navbar-end">
+                @guest
+                <div class="navbar-item">
+                    <div class="buttons">
+                        <a href="#login" class="button is-light">Einloggen</a>
+                    </div>
+                </div>
+                @endguest
+
+            </div>
+        </div>
+    </nav>
+        @yield('maincontents')
+
+</body>
+
+</html>
---
+++ resources/views/tlog.blade.php
@@ -0,0 +1,28 @@
+@extends('layouts.larablog')
+
+@section('maincontents')
+<div class="container">
+    <div class="columns">
+        <div class="column">
+            <p>
+                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Porro nemo labore obcaecati quis rerum unde, nobis incidunt? Quidem repellat dolores dolorem cupiditate deserunt fugiat. Suscipit a aliquid soluta qui tempora!
+                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Assumenda iure impedit ea nemo recusandae ad sequi quisquam natus eos quidem! Voluptatum quidem nesciunt voluptas molestiae perspiciatis molestias explicabo eveniet quod!
+            </p>
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt temporibus vitae autem laudantium, quod qui aspernatur dignissimos quam totam ullam maiores quo dolor ex in soluta nobis corrupti blanditiis sunt.
+            </p>
+        </div>
+        <div class="column">
+            <p>
+                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Veritatis, numquam. Obcaecati quo ratione ex illum commodi veniam vitae, beatae nam blanditiis excepturi libero molestias! Sit quidem rem obcaecati sed! Impedit. Lorem ipsum dolor sit, amet consectetur adipisicing elit. Assumenda obcaecati dolor molestiae, necessitatibus aut iure, odit in quidem debitis atque distinctio quisquam praesentium nulla. Omnis tenetur autem suscipit placeat at.
+                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quis, quos fuga. Itaque, adipisci. Ea sit odio nesciunt at deserunt, ad nisi totam, nihil architecto inventore vero dolor, error eius ex.
+            </p>
+            <p>
+                Lorem ipsum dolor sit, amet consectetur adipisicing elit. Porro debitis optio quidem iusto eaque, earum molestiae ut obcaecati non eos illo, quae modi unde id facilis voluptate cum eum culpa!
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium repudiandae doloremque, iusto rerum nulla a possimus quas reprehenderit beatae voluptatem error et nam placeat itaque maiores amet voluptatibus. Rem, corrupti!
+
+            </p>
+        </div>
+    </div>
+</div>
+@endsection
