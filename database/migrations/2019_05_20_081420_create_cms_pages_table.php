e33ca1cf71da6ad7ac93b26a8e783ad0301df9b2
Marcel Schindler <marcel.schindler@Lobo.local>
2019-05-22 14:30:11 +0200

    Better Startpage

--- database/migrations/2019_05_20_081420_create_cms_pages_table.php
+++ database/migrations/2019_05_20_081420_create_cms_pages_table.php
@@ -22,30 +22,16 @@ class CreateCmsPagesTable extends Migration
         });
         DB::table('cms_pages')->insert(['filename'=>'home', 'title'=>'Hallo!', 'contents'=>'
 <div class="row">
-    <div class="four columns">
-        <h2 clasS="title primary">Funktionen</h2>
+<div class="six columns">
+        <p>This is your startpage. Tlog5 needs a startpage, it won´t run without one. The reason behind this is, that we need to be able to have a non-blogging-approach
+        here.</p>
         <p>
-            Vollständige Bloglösung für mehrere Autoren, komplett aufgesetzt auf Laravel und Jquery. Kompromisslos schnell und sicher.
+        The plan is that you can call "static" pages as well, which will be made out of Database-Content
         </p>
-        <ol>
-            <li>Beliebig viele Blogposts</li>
-            <li>Beliebig viele Kategorien, Tags</li>
-            <li>RSS-Feeds.</li>
-            <li>Kommentare. (*geplant*)</li>
-        </ol>
-    </div>
-    <div class="four columns">
-        <h2 clasS="title primary">NOch mehr Content</h2>
-        <p>
-        Bla blabla bla
-        </p>
-    </div>
-    <div class="four columns">
-        <h2 clasS="title primary">Funktionen</h2>
-        <p>
-        Ganz viel.
-        </p>
-    </div>
+</div>
+<div class="six columns">
+When you have exhausted <b>all</b> possibilites, remember this: <em>you haven\'t</em>
+</div>
 </div>
         ']);
     }
