1c1513b7f8da138fff43b3cfc4927f58e3a8dda6
Marcel Schindler <marcel.schindler@lobo-tueren.de>
2019-06-07 15:38:03 +0200

    Modified Main Menu.

--- resources/views/partial/mainmenu.blade.php
+++ resources/views/partial/mainmenu.blade.php
@@ -2,7 +2,5 @@
     <a href="{{ url('/')}}">Home</a>
     <a href="{{ url('blog/')}}">Blog</a>
     <a href="{{ url('impressum') }}">Impressum</a>
-    <a href="{{ url('music')}}">Meine Musik</a>
     <a href="#" class="icon drawer">&#9776;</a>
-
 </div>
