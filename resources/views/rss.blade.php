2ee7f99e37618aa23de05415ff9cc0a999e48070
marcshake <marcshake@gmail.com>
2019-06-11 20:42:14 +0200

    RFC-Date

--- resources/views/rss.blade.php
+++ resources/views/rss.blade.php
@@ -8,14 +8,14 @@
   @foreach ($posts as $item)
   <item>
     <title>{{$item->title}}</title>
-    <link>{{$item->link}}</link>
+    <link>{{$item->link}}/</link>
     <image>
     <url>{{$item->mainImage ? asset('storage/thumbnail/'.$item->mainImagePath->filename): asset('images/wall.jpg')}}</url>
     <title>{{$item->title}}</title>
     
     </image>
     <pubDate>
-    {{$item->created_at->toIso8601String('%d.%m.%Y')}}
+    {{$item->created_at->toRfc822String('%d.%m.%Y')}}
     </pubDate>
     <description><![CDATA[{{$item->contents}} ]]></description>
   </item>
