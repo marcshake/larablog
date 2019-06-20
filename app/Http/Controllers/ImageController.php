44539c35dfe308fb794f2a4ec8f677200a07c97e
Marcel Schindler <info@trancefish.de>
2019-06-20 10:44:26 +0200

    Modified thumbnail size to 16:9

--- app/Http/Controllers/ImageController.php
+++ app/Http/Controllers/ImageController.php
@@ -68,7 +68,7 @@ class ImageController extends Controller
         $file->storeAs('public/thumbnail', $originalName);
         $path = public_path('storage/thumbnail/'.$originalName);
         $pathtiny = public_path('storage/thumbnail/tiny_'.$originalName);
-        $iM->make($path)->fit(400)->save($path);
+        $iM->make($path)->fit(400, 225)->save($path);
         $iM->make($path)->fit(200)->save($pathtiny);
         $media = new Media;
         $media->filename = $originalName;
