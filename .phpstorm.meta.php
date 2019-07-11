c765497b2f64f2214fcd46abca7f8d944218a271
Marcel Schindler <info@trancefish.de>
2019-07-11 12:29:04 +0200

    Darkmode

--- .phpstorm.meta.php
+++ .phpstorm.meta.php
@@ -5,7 +5,7 @@ namespace PHPSTORM_META {
 
    /**
     * PhpStorm Meta file, to provide autocomplete information for PhpStorm
-    * Generated on 2019-07-11 10:47:57.
+    * Generated on 2019-07-11 12:28:40.
     *
     * @author Barry vd. Heuvel <barryvdh@gmail.com>
     * @see https://github.com/barryvdh/laravel-ide-helper
