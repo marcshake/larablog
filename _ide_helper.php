a56bf89000a6561efd7d6da7d43c09e26f6bf6c2
marcshake <marcshake@gmail.com>
2019-07-11 20:48:22 +0200

    Dark Theme

--- _ide_helper.php
+++ _ide_helper.php
@@ -3,7 +3,7 @@
 
 /**
  * A helper file for Laravel 5, to provide autocomplete information to your IDE
- * Generated for Laravel 5.8.28 on 2019-07-11 12:28:40.
+ * Generated for Laravel 5.8.28 on 2019-07-11 20:29:32.
  *
  * This file should not be included in your code, only analyzed by your IDE!
  *
