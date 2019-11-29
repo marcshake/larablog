c0d37deac60cb33ed7e9bedc7823c4221dc93e22
Marcel Schindler <info@trancefish.de>
2019-11-29 10:00:38 +0100

    Added some routes

--- routes/web.php
+++ routes/web.php
@@ -18,38 +18,42 @@ Route::get('blog/{title?}/{id?}', 'BlogController@index');
 Route::get('tag/{name?}', 'BlogController@tag');
 Route::get('category/{name?}', 'BlogController@category');
 Route::get('logout', 'Auth\LoginController@logout');
-Route::get('admin', 'AdminController@index');
-Route::get('admin/blogs', 'AdminController@list');
-Route::get('admin/edit/{id}', 'AdminController@edit');
-Route::get('admin/new', 'AdminController@create');
-Route::get('admin/delete/{id}', 'AdminController@destroy');
-Route::get('admin/status/{id}', 'AdminController@status');
 Route::get('sitemap.xml', 'SitemapController@index');
-Route::get('music', 'MusicController@index');
-Route::get('music/download/{song_id}', 'MusicController@download');
+/*Route::get('music', 'MusicController@index');
+Route::get('music/download/{song_id}', 'MusicController@download');*/
+
 Route::get('rss', 'RssController@index');
 Route::get('preview/{title}/{id}', 'PreviewController@index');
 
-Route::get('admin/filer', 'ImageController@index');
-Route::get('admin/cms', 'AdminCMSController@index');
-Route::get('admin/cms/new', 'AdminCMSController@create');
-Route::get('admin/cms/edit/{id}', 'AdminCMSController@edit');
-Route::get('admin/cms/status/{id}', 'AdminCMSController@status');
-Route::get('admin/cms/delete/{id}', 'AdminCMSController@destroy');
-Route::get('admin/user/', 'UserManagerController@index');
-Route::get('admin/user/edit/{id}', 'UserManagerController@edit');
+Route::get('admin', 'AdminController@index');
 
+//Admin-Routes
+Route::prefix('admin')->group(function () {
+    Route::get('blogs', 'AdminController@list');
+    Route::get('edit/{id}', 'AdminController@edit');
+    Route::get('new', 'AdminController@create');
+    Route::get('delete/{id}', 'AdminController@destroy');
+    Route::get('status/{id}', 'AdminController@status');
+    Route::get('filer', 'ImageController@index');
+    Route::get('cms', 'AdminCMSController@index');
+    Route::get('cms/new', 'AdminCMSController@create');
+    Route::get('cms/edit/{id}', 'AdminCMSController@edit');
+    Route::get('cms/status/{id}', 'AdminCMSController@status');
+    Route::get('cms/delete/{id}', 'AdminCMSController@destroy');
+    Route::get('user/', 'UserManagerController@index');
+    Route::get('user/edit/{id}', 'UserManagerController@edit');
+    Route::post('update/{id}', 'AdminController@update');
+    Route::post('save', 'AdminController@store');
+    Route::post('filer', 'ImageController@store');
+    Route::post('cms/edit/{id}', 'AdminCMSController@update');
+    Route::post('cms/new', 'AdminCMSController@store');
+    Route::post('user/edit/{id}', 'UserManagerController@update');
+});
 // All other Routes
 Route::get('/{slug?}', 'CMSController@index');
 
-Route::post('admin/update/{id}', 'AdminController@update');
-Route::post('admin/save', 'AdminController@store');
-Route::post('admin/filer', 'ImageController@store');
 Route::post('ajax/image/', 'ImageController@gallery');
 Route::post('ajax/loadImage/{id}', 'ImageController@ajaxImage');
 Route::post('ajax/deleteImage/{id}', 'ImageController@deleteImage');
-Route::post('admin/cms/edit/{id}', 'AdminCMSController@update');
-Route::post('admin/cms/new', 'AdminCMSController@store');
-Route::post('admin/user/edit/{id}', 'UserManagerController@update');
 
 Route::post('search/', 'BlogController@Search');
