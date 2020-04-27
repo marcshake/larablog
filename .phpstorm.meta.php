5bef403a3a8345304665b060d8d2f9d4ca841080
Marcel Schindler <marcel.schindler@lobo-tueren.de>
2020-04-27 08:19:39 +0000

    Updates

--- .phpstorm.meta.php
+++ .phpstorm.meta.php
@@ -5,7 +5,7 @@ namespace PHPSTORM_META {
 
    /**
     * PhpStorm Meta file, to provide autocomplete information for PhpStorm
-    * Generated on 2020-04-11 11:21:25.
+    * Generated on 2020-04-27 10:16:54.
     *
     * @author Barry vd. Heuvel <barryvdh@gmail.com>
     * @see https://github.com/barryvdh/laravel-ide-helper
