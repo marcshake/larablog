3073b5e3184239354ceeacc3716c31e6f01aee41
Marcel Schindler <info@trancefish.de>
2020-06-12 11:11:03 +0200

    Dark theme

--- resources/views/theme2020/home.blade.php
+++ resources/views/theme2020/home.blade.php
@@ -3,24 +3,7 @@
 
   @section('maincontents')
 
-
-  <header>
-    <div class="overlay"></div>
-    <video playsinline="playsinline" autoplay="autoplay" muted="muted" loop="loop">
-      <source src="{{asset('images/tunnelmotionstunnel.mp4')}}" type="video/mp4">
-    </video>
-    <div class="container h-100">
-      <div class="d-flex h-100 text-center align-items-center">
-        <div class="w-100 text-white">
-          <h1 class="display-3">{{$home->title}}</h1>
-          <p class="lead mb-0">
-            {!!$home->contents!!}
-
-          </p>
-        </div>
-      </div>
-    </div>
-  </header>
+@include('theme2020.video')
 
 <div class="container mt-4">
 
