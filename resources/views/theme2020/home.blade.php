860b172aeb72cdc965775fd8c9528fa8b49dbc6e
Marcel Schindler <info@trancefish.de>
2020-06-12 10:37:50 +0200

    Header-Video / Placeholder

--- resources/views/theme2020/home.blade.php
+++ resources/views/theme2020/home.blade.php
@@ -1,89 +1,49 @@
-<!DOCTYPE html>
-<html lang="de">
+@extends('theme2020.layout')
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <meta property="og:url" content="{{Request::fullUrl()}}">
-    <meta property="og:type" content="website">
-    <meta property="og:title" content="@yield('title')">
-    <meta name="csrf-token" content="{{ csrf_token() }}">
-    <meta name="description" content="{{$metadescription ?? env('DESCRIPTION','')}}">
-    <link rel="stylesheet" href="{{ asset('css/theme2020.css')}}">
 
-    @yield('opengraph')
-    <title>@yield('title') - </title>
-</head>
+  @section('maincontents')
 
-<body>
 
-    <!-- Navigation -->
-    <nav class="navbar navbar-expand-lg navbar-dark bg-dark static-top">
-        <div class="container">
-            <a class="navbar-brand" href="#">
-                Larablog
-            </a>
-            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive"
-                aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse" id="navbarResponsive">
-                <ul class="navbar-nav ml-auto">
-                    <li class="nav-item active">
-                        <a class="nav-link" href="#">Home
-                        </a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="/blog">Blog</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="/ueber-mich">Services</a>
-                    </li>
+  <header>
+    <div class="overlay"></div>
+    <video playsinline="playsinline" autoplay="autoplay" muted="muted" loop="loop">
+      <source src="{{asset('images/tunnelmotionstunnel.mp4')}}" type="video/mp4">
+    </video>
+    <div class="container h-100">
+      <div class="d-flex h-100 text-center align-items-center">
+        <div class="w-100 text-white">
+          <h1 class="display-3">{{$home->title}}</h1>
+          <p class="lead mb-0">
+            {!!$home->contents!!}
 
-                </ul>
-            </div>
+          </p>
         </div>
-    </nav>
+      </div>
+    </div>
+  </header>
 
-    <div class="container">
+<div class="container mt-4">
 
-        <h1 class="my-4"> {{$home->title}}
-            <small>Neu im Blog</small>
-        </h1>
-
-        <div class="row">
-
-            @foreach ($morePosts as $items)
-
-            <div class="col-lg-4 col-sm-6 mb-4">
-                <div class="card h-100">
-                    <a href="{{url('blog/'.$items->url,$items->id)}}">
-                    {{--     <img class="card-img-top"
-                            src="{{$items->mainImage ? asset('storage/thumbnail/tiny_'.$items->mainImagePath->filename): asset('images/wall.jpg')}}"
-                            alt="{{$items->title}}">--}}
-                            <img class="card-img-top"
-                            src="//trancefish.de/storage/thumbnail/tiny_{{$items->mainImagePath->filename}}"
-                            alt="{{$items->title}}">
-                        </a>
-                    <div class="card-body">
-                        <h4 class="card-title">
-                            <a href="{{url('blog/'.$items->url,$items->id)}}">{{$items->title}}</a>
-                        </h4>
-                        <p class="card-text">
-                            {!!$items->contents!!}
-                        </p>
-                    </div>
+    <div class="row">
+        @foreach ($morePosts as $items)
+        <div class="col-lg-4 col-sm-6 mb-4">
+            <div class="card h-100">
+                <a href="{{url('blog/'.$items->url,$items->id)}}">
+                    <img class="card-img-top"
+                        src="{{$items->mainImage ? asset('storage/thumbnail/tiny_'.$items->mainImagePath->filename): asset('images/wall.jpg')}}"
+                        alt="{{$items->title}}">
+                </a>
+                <div class="card-body">
+                    <h4 class="card-title">
+                        <a href="{{url('blog/'.$items->url,$items->id)}}">{{$items->title}}</a>
+                    </h4>
+                    <p class="card-text">
+                        {!!$items->contents!!}
+                    </p>
                 </div>
             </div>
-            @endforeach
         </div>
-
-
-
+        @endforeach
     </div>
-
-    <script src="{{asset('js/theme2020.js')}}"></script>
-</body>
-
-</html>
+</div>
+@endsection
---
+++ resources/views/theme2020/layout.blade.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="de">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <meta property="og:url" content="{{Request::fullUrl()}}">
+    <meta property="og:type" content="website">
+    <meta property="og:title" content="@yield('title')">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+    <meta name="description" content="{{$metadescription ?? env('DESCRIPTION','')}}">
+    <link rel="stylesheet" href="{{ asset('css/theme2020.css')}}">
+    @yield('opengraph')
+    <title>@yield('title') - </title>
+</head>
+
+<body>
+    <!-- Navigation -->
+    @include('theme2020.navigation')
+    @yield('maincontents')
+    <script src="{{asset('js/theme2020.js')}}"></script>
+</body>
+
+</html>
