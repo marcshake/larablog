9fd41a09e6751b246003f0814cbc413f4cda6cf9
Marcel Schindler <marcel.schindler@lobo-tueren.de>
2020-06-17 11:25:30 +0000

    Headline as link

--- resources/views/theme2020/blog.blade.php
+++ resources/views/theme2020/blog.blade.php
@@ -4,51 +4,55 @@
 
 @section('opengraph')
 
-@forelse ($blogposts as $item)
-<meta property="og:image"
-    content="{{$item->mainImage ? asset('storage/thumbnail/'.$item->mainImagePath->filename): asset('images/wall.jpg')}}">
-@empty
+    @forelse ($blogposts as $item)
+        <meta property="og:image"
+              content="{{$item->mainImage ? asset('storage/thumbnail/'.$item->mainImagePath->filename): asset('images/wall.jpg')}}">
+    @empty
 
-@endforelse
+    @endforelse
 @endsection
 
 @section('maincontents')
-<div class="container">
+    <div class="container">
 
-    <!-- Page Heading -->
-    <h1 class="my-4">Blogposts
-      <small>Liste</small>
-    </h1>
+        <!-- Page Heading -->
+        <h1 class="my-4">Blogposts
+            <small>Liste</small>
+        </h1>
     @forelse ($blogposts as $item)
 
-    <!-- Project One -->
-    <div class="row">
-      <div class="col-md-5">
-        <a href="{{url('blog/'.$item->url,$item->id)}}">            
-          <img data-src="{{$item->mainImage ? asset('storage/thumbnail/'.$item->mainImagePath->filename): asset('images/wall.jpg')}}"
-            class="img-fluid" alt="{{$item->title}}">
-        </a>
-      </div>
-      <div class="col-md-7">
-        <h3>
-            {{$item->title}}
-        </h3>
-        {!! $item->shortcontents !!}
-        <a href="{{url('blog/'.$item->url,$item->id)}}">
+        <!-- Project One -->
+            <div class="row">
+                <div class="col-md-5">
+                    <a href="{{url('blog/'.$item->url,$item->id)}}">
+                        <img
+                            data-src="{{$item->mainImage ? asset('storage/thumbnail/'.$item->mainImagePath->filename): asset('images/wall.jpg')}}"
+                            class="img-fluid" alt="{{$item->title}}">
+                    </a>
+                </div>
+                <div class="col-md-7">
+                    <a href="{{url('blog/'.$item->url,$item->id)}}">
 
-        Weiter
-        </a>
-      </div>
+                        <h3>
+                            {{$item->title}}
+                        </h3>
+                    </a>
+                    {!! $item->shortcontents !!}
+                    <a href="{{url('blog/'.$item->url,$item->id)}}">
+
+                        Weiter
+                    </a>
+                </div>
+            </div>
+            <!-- /.row -->
+        @empty
+            Dein Blog ist leer.
+
+            <hr>
+        @endforelse
+        {{$blogposts->links()}}
+
+
     </div>
-    <!-- /.row -->
-    @empty
-    Dein Blog ist leer.
-
-    <hr>
-    @endforelse
-    {{$blogposts->links()}}
-
-
-</div>
-  <!-- /.container -->
+    <!-- /.container -->
 @endsection
