5dd0cf4483d4196b2f5b4c4521f4016f3960e090
Marcel Schindler <marcel.schindler@lobo-tueren.de>
2020-07-24 08:39:42 +0000

    Removed Centering

--- resources/views/theme2020/video.blade.php
+++ resources/views/theme2020/video.blade.php
@@ -4,7 +4,7 @@
       <source src="{{asset('images/tunnelmotionstunnel.mp4')}}" type="video/mp4">
     </video>
     <div class="container h-100">
-      <div class="d-flex h-100 text-center align-items-center">
+      <div class="d-flex h-100 align-items-center">
         <div class="w-100 text-white">
           <h1>{{$home->title}}</h1>
           <p class="lead mb-0">
