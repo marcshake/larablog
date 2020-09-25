1804ec2da549e9c266fba2e3009fe6f1d5737f71
Marcel Schindler <marcel.schindler@lobo-tueren.de>
2020-09-25 06:50:46 +0000

    Tiny updates

--- resources/js/admin.js
+++ resources/js/admin.js
@@ -1,20 +1,17 @@
 window.$ = window.jQuery = require('jquery');
-
-
-$(document).ready(function () {
+$(document).ready(function() {
     $.ajaxSetup({
         headers: {
             'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
         }
     });
-
     /**
      * Browse Images
      */
-    var myImagelib = function () {
+    var myImagelib = function() {
         console.log('Called Imagebrowser');
         $('#modalTitle').html('Bild auswählen');
-        $.post('/ajax/image', function (data) {
+        $.post('/ajax/image', function(data) {
             $('#modalContents').html(data);
         });
     };
@@ -22,70 +19,64 @@ $(document).ready(function () {
      * Use Specific Image
      * @param {} item
      */
-    var useImage = function (item) {
+    var useImage = function(item) {
         var img = item.data('item');
         $('#imageID').val(img);
-        $.post('/ajax/loadImage/' + img, function (imageDetail) {
-            $('#previewImag').html('<img src="/storage/thumbnail/tiny_' + imageDetail['thumbnail'] + '">');
+        $.post('/ajax/loadImage/' + img, function(imageDetail) {
+            $('#previewImag').html('<img src="/storage/thumbnail/tiny_' + imageDetail['thumbnail'] +
+                '">');
             return false;
         });
     }
-
-    var imageDetails = function (item) {
+    var imageDetails = function(item) {
         var img = item.data('item');
         $('#imageID').val(img);
-        $.post('/ajax/loadImage/' + img, function (imageDetail) {
+        $.post('/ajax/loadImage/' + img, function(imageDetail) {
             var path = '/storage/thumbnail/tiny_' + imageDetail['thumbnail'];
-            var fullpath = window.location.protocol + '//' + window.location.host + '/storage/uploads/' + imageDetail['thumbnail'];
-            var myhtml = '<figure><img class="u-full-width" src="' + fullpath + '" /><figcaption>' + fullpath + '</figcaption></figure>';
-            myhtml += '<button id="deleteImage" data-id="' + imageDetail['id'] + '">Löschen</button>';
+            var fullpath = window.location.protocol + '//' + window.location.host +
+                '/storage/uploads/' + imageDetail['thumbnail'];
+            var myhtml = '<figure><img class="u-full-width" src="' + fullpath + '" /><figcaption>' +
+                fullpath + '</figcaption></figure>';
+            myhtml += '<button id="deleteImage" data-id="' + imageDetail['id'] +
+                '">Löschen</button>';
             $('#modalTitle').html('Bilddetails');
             $('#modalContents').html(myhtml);
             $('.modal').removeClass('hidden');
             return false;
         });
-
     }
-
-
-    var deleteImage = function (item) {
+    var deleteImage = function(item) {
         var image = item.data('id');
-        $.post('/ajax/deleteImage/' + image, function (data) {
+        $.post('/ajax/deleteImage/' + image, function(data) {
             $('[data-item=' + image + ']').hide();
             $('.modal').addClass('hidden');
         });
     }
-
     /**
      * All the Handlers
      */
-    $('.close-modal').click(function () {
+    $('.close-modal').click(function() {
         $('.modal').addClass('hidden')
     });
-    $('.open-modal').click(function () {
+    $('.open-modal').click(function() {
         $('.modal').removeClass('hidden')
     });
-    $('.ibrowser').on('click', '.thumb', function () {
+    $('.ibrowser').on('click', '.thumb', function() {
         useImage($(this))
     });
-    $('.idetails').on('click', '.thumb', function () {
+    $('.idetails').on('click', '.thumb', function() {
         imageDetails($(this))
     });
-    $('#modalContents').on('click', '#deleteImage', function () {
+    $('#modalContents').on('click', '#deleteImage', function() {
         deleteImage($(this))
     });
-    $('.imageBrowser').click(function () {
+    $('.imageBrowser').click(function() {
         myImagelib()
     })
     $('.alert').delay(5000).fadeOut('slow');
-
-    $('#generator').click(function () {
+    $('#generator').click(function() {
         let r = Math.random().toString(36).substring(7);
         $('#password').val(r);
         return false;
     });
-
 });
-
-
-
--- resources/js/theme2020.js
+++ resources/js/theme2020.js
@@ -1,27 +1,21 @@
 require('bootstrap');
 
-
 function para(thatID, factor) {
     let intro = document.getElementById(thatID);
     if (intro === null) {
         return false
     }
-
-
-    window.onscroll = function () {
+    window.onscroll = function() {
         let w = Math.max(document.documentElement.clientWidth, window.innerWidth || 0);
         let scrollBarPosition;
         scrollBarPosition = (window.pageYOffset | document.body.scrollTop);
         if (w > 550) {
-
             let scroll = (scrollBarPosition) * factor * -1;
             intro.style.backgroundPositionY = scroll + "px"
         }
     }
 }
-
-
-window.addEventListener("load", function () {
+window.addEventListener("load", function() {
     para('parascroll', .5)
     var allimages = document.getElementsByTagName('img');
     for (var i = 0; i < allimages.length; i++) {
