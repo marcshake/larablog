7e4bda77a9814fbc6f7a0e98990de9fee04d08af
Marcel Schindler <info@trancefish.de>
2021-11-11 10:31:47 +0100

    localization

---
+++ resources/views/admin/navigation.blade.php
@@ -0,0 +1,38 @@
+<nav>
+    <ul>
+        <li><a href="#">{{ __('dashboard') }}</a></li>
+        <li>{{ __('blog') }}
+            <ul>
+                <li><a href="#">{{ __('List of Entries') }}</a></li>
+                <li><a href="#">{{ __('Create New') }}</a></li>
+                <li><a href="#">{{ __('Blog Categories') }}</a></li>
+                <li><a href="#">{{ __('Keywords') }}</a></li>
+            </ul>
+
+        </li>
+        <li>{{ __('media') }}
+            <ul>
+                <li>
+                    <a href="#">{{ __('Media List') }}</a>
+                </li>
+                <li>
+                    <a href="#">{{ __('Add Media') }}</a>
+                </li>
+            </ul>
+
+        </li>
+        <li>{{ __('Pages') }}
+            <ul>
+                <li>
+                    <a href="#">{{ __('All Pages') }}</a>
+                </li>
+                <li>
+                    <a href="#">{{ __('Add Page') }}</a>
+                </li>
+                <li>
+                    <a href="#">{{ __('All Pages') }}</a>
+                </li>
+            </ul>
+        </li>
+    </ul>
+</nav>
