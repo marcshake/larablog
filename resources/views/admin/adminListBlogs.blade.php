8e7ccc14953e34aa4d69b5334f81dd096d91e9ce
Marcel Schindler <info@trancefish.de>
2021-11-13 13:52:45 +0100

    More localization

--- resources/views/admin/adminListBlogs.blade.php
+++ resources/views/admin/adminListBlogs.blade.php
@@ -1,17 +1,63 @@
 @extends('admin.administration')
 @section('title')
-    {{__('List of Blogs')}}
+    {{ __('List of Blogs') }}
 @endsection
 @section('contents')
-<table class="table u-full-width">
-@foreach ($collection as $item)
-    <tr>
-        <td><a href="{{url('admin/edit',$item->id)}}">
-            {{ $item->title}}
-        </a>
-    </td>
-    </tr>    
-@endforeach
-</table>
+    <table class="table is-striped is-hoverable">
+        <thead>
+            <tr>
+                <th>{{ __('number') }}</th>
+                <th>{{ __('date') }}</th>
+                <th>{{ __('entrytitle') }}</th>
+                <th>{{ __('author') }}</th>
+                <th>{{ __('tags') }}</th>
+                <th>{{ __('categories') }}</th>
+            </tr>
+        </thead>
 
+        <tbody>
+            @foreach ($collection as $item)
+                <tr>
+                    <td>{{ $loop->iteration }}</td>
+                    <td>{{ $item->created_at->formatLocalized('%d.%m.%Y') }}
+                        @if ($item->visible == 0)
+                            <span class="tag is-light">{{ __('draft') }}</span>
+                        @endif
+
+                    </td>
+                    <td>
+                        <a href="{{ url('admin/edit', $item->id) }}">
+                            {{ $item->title }}
+                        </a>
+                        <span class="subaction">
+                            <a href="{{ url('admin/delete', $item->id) }}">Löschen</a>
+                            <a target="_blank" href="{{ url('preview/' . $item->url, $item->id) }}">{{ __('preview') }}</a>
+                            <a href="{{ url('admin/status', $item->id) }}">
+                                {{ $item->visible ? __('hide') : __('publish') }}
+                            </a>
+
+                        </span>
+                    </td>
+                    <td>{{ $item->authorname->name }}</td>
+                    <td>
+                        @forelse ($item->Tags as $tags)
+                            <span class="tag">{{ $tags->tag }}</span>
+                        @empty
+
+                        @endforelse
+                    </td>
+                    <td>
+                        @forelse ($item->categories as $cats)
+                            <span class="tag">{{ $cats->name }}</span>
+                        @empty
+
+                        @endforelse
+
+
+                    </td>
+                </tr>
+            @endforeach
+        </tbody>
+    </table>
+    {{ $collection->links() }}
 @endsection
--- resources/views/admin/navigation.blade.php
+++ resources/views/admin/navigation.blade.php
@@ -1,10 +1,8 @@
 <nav>
-
-
     <ul>
         <li>{{ __('blog') }}<ul>
-                <li><a href="{{url('admin/blogs')}}">{{ __('List of Entries') }}</a></li>
-                <li><a href="{{url('admin/new')}}">{{ __('Create New') }}</a></li>
+                <li><a href="{{ url('admin/blogs') }}">{{ __('List of Entries') }}</a></li>
+                <li><a href="{{ url('admin/new') }}">{{ __('Create New') }}</a></li>
                 <li><a href="#">{{ __('Blog Categories') }}</a></li>
                 <li><a href="#">{{ __('Keywords') }}</a></li>
             </ul>
@@ -16,13 +14,11 @@
             </ul>
         </li>
         <li>{{ __('Pages') }}<ul>
-
                 <li><a href="#">{{ __('All Pages') }}</a></li>
                 <li><a href="#">{{ __('Add Page') }}</a></li>
                 <li><a href="#">{{ __('All Pages') }}</a></li>
             </ul>
         </li>
-
         </li>
     </ul>
 </nav>
