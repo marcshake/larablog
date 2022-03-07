978bc1c20905e7860fb5a9396ff6e72803643979
Marcel Schindler <info@trancefish.de>
2022-03-07 17:01:34 +0100

    Changed Loading

--- resources/views/theme2021/layout.blade.php
+++ resources/views/theme2021/layout.blade.php
@@ -10,8 +10,8 @@
     <meta property="og:title" content="@yield('title')">
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <meta name="description" content="{{ $metadescription ?? env('DESCRIPTION', '') }}">
-    <link rel="stylesheet" href="{{ mix('css/theme2022.css') }}">
-    <script src="{{mix('js/theme2022.js')}}"></script>
+
+    <script src="{{ mix('js/theme2022.js') }}"></script>
     @yield('opengraph')
     <title>@yield('title') </title>
 </head>
@@ -22,6 +22,12 @@
         @yield('contents')
     </div>
     @include('theme2021.footer')
+
+    <link rel="stylesheet" href="{{ mix('css/theme2022.css') }}">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Open+Sans&family=Rowdies:wght@700&display=swap"
+        rel="stylesheet">
 </body>
 
 </html>
