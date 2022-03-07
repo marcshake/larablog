d2c02d439ffc7a538f42d220751c944c33ceeb66
Marcel Schindler <info@trancefish.de>
2022-03-07 17:04:17 +0100

    Bugfix Theme

--- public/js/admin.js
+++ public/js/admin.js
@@ -11106,8 +11106,8 @@ __webpack_require__.r(__webpack_exports__);
 /******/ 		// [resolve, reject, Promise] = chunk loading, 0 = chunk loaded
 /******/ 		var installedChunks = {
 /******/ 			"/js/admin": 0,
-/******/ 			"css/theme2022": 0,
-/******/ 			"css/admin": 0
+/******/ 			"css/admin": 0,
+/******/ 			"css/theme2022": 0
 /******/ 		};
 /******/ 		
 /******/ 		// no chunk on demand loading
@@ -11157,9 +11157,9 @@ __webpack_require__.r(__webpack_exports__);
 /******/ 	// startup
 /******/ 	// Load entry module and return exports
 /******/ 	// This entry module depends on other loaded chunks and execution need to be delayed
-/******/ 	__webpack_require__.O(undefined, ["css/theme2022","css/admin"], () => (__webpack_require__("./resources/js/admin.js")))
-/******/ 	__webpack_require__.O(undefined, ["css/theme2022","css/admin"], () => (__webpack_require__("./resources/sass/theme2022.scss")))
-/******/ 	var __webpack_exports__ = __webpack_require__.O(undefined, ["css/theme2022","css/admin"], () => (__webpack_require__("./resources/sass/admin.scss")))
+/******/ 	__webpack_require__.O(undefined, ["css/admin","css/theme2022"], () => (__webpack_require__("./resources/js/admin.js")))
+/******/ 	__webpack_require__.O(undefined, ["css/admin","css/theme2022"], () => (__webpack_require__("./resources/sass/theme2022.scss")))
+/******/ 	var __webpack_exports__ = __webpack_require__.O(undefined, ["css/admin","css/theme2022"], () => (__webpack_require__("./resources/sass/admin.scss")))
 /******/ 	__webpack_exports__ = __webpack_require__.O(__webpack_exports__);
 /******/ 	
 /******/ })()
